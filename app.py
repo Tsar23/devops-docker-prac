27ad41cf45a261097d279821af613b1e90ba9467
Mac <mac@Macs-MacBook-Pro.local>
2026-06-08 11:00:58 +0300

    Initial commit: added Dockerfile, app.py and README.md

---
+++ app.py
@@ -0,0 +1,11 @@
+import time
+
+def main():
+    print("DevOps Service is running...")
+    print("Student: Tsar Volodymyr | Group: KN-32sp")
+    while True:
+        print("Processing data...")
+        time.sleep(5)
+
+if __name__ == "__main__":
+    main()
